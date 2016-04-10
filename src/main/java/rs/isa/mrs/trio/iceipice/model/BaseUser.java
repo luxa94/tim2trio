e23fd52ee7b8676443c859f0aff60233c4fc9f0f
Nikola Lukić <luknik94@gmail.com>
2016-04-10 12:50:02 +0200

    model example - base user

---
+++ src/main/java/rs/isa/mrs/trio/iceipice/model/BaseUser.java
@@ -0,0 +1,92 @@
+package rs.isa.mrs.trio.iceipice.model;
+
+import javax.persistence.*;
+import java.util.Date;
+
+/**
+ * Created by nikolalukic on 4/10/16.
+ */
+@Entity
+@Table(name = "base_user")
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+public class BaseUser {
+
+    @Id
+    @GeneratedValue
+    private long id;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "surname", nullable = false)
+    private String surname;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    @Column(name = "birth_date")
+    private Date birthDate;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+    
+}
