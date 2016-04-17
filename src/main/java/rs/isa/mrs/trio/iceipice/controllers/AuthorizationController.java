329c4b10608d6c778e1f9aa645e3a1f80c021bee
Nikola Lukić <luknik94@gmail.com>
2016-04-17 12:46:45 +0200

    unnecesery authorization stuff removed

--- src/main/java/rs/isa/mrs/trio/iceipice/controllers/AuthorizationController.java
+++ src/main/java/rs/isa/mrs/trio/iceipice/controllers/AuthorizationController.java
@@ -18,28 +18,10 @@ import rs.isa.mrs.trio.iceipice.repository.*;
 public class AuthorizationController {
 
     @Autowired
-    BartenderRepository bartenderRepository;
-
-    @Autowired
     BaseUserRepository baseUserRepository;
 
     @Autowired
-    CookRepository cookRepository;
-
-    @Autowired
     GuestRepository guestRepository;
-
-    @Autowired
-    ProviderRepository providerRepository;
-
-    @Autowired
-    RestaurantManagerRepository restaurantManagerRepository;
-
-    @Autowired
-    SystemManagerRepository systemManagerRepository;
-
-    @Autowired
-    WaiterRepository waiterRepository;
 
     @RequestMapping(value = "/login", method = RequestMethod.POST)
     public ResponseEntity login(@RequestBody LoginDTO loginDTO) {
@@ -49,24 +31,6 @@ public class AuthorizationController {
 
         BaseUser baseUser = baseUserRepository.findByEmailAndPassword(email, password);
         if (baseUser != null) {
-//            if (baseUser.getType().equals(UserTypes.BARTENDER)) {
-//                baseUser = bartenderRepository.findByEmailAndPassword(email, password);
-//            } else if (baseUser.getType().equals(UserTypes.COOK)) {
-//                baseUser = cookRepository.findByEmailAndPassword(email, password);
-//            } else if (baseUser.getType().equals(UserTypes.GUEST)) {
-//                baseUser = guestRepository.findByEmailAndPassword(email, password);
-//            } else if (baseUser.getType().equals(UserTypes.PROVIDER)) {
-//                baseUser = providerRepository.findByEmailAndPassword(email, password);
-//            } else if (baseUser.getType().equals(UserTypes.RESTAURANT_MANAGER)) {
-//                baseUser = restaurantManagerRepository.findByEmailAndPassword(email, password);
-//            } else if (baseUser.getType().equals(UserTypes.SYSTEM_MANAGER)) {
-//                baseUser = systemManagerRepository.findByEmailAndPassword(email, password);
-//            } else if (baseUser.getType().equals(UserTypes.WAITER)) {
-//                baseUser = waiterRepository.findByEmailAndPassword(email, password);
-//            } else {
-//                System.out.println(String.format("Unknown user type: %s", baseUser.getType()));
-//                return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
-//            }
             return new ResponseEntity<>(baseUser, HttpStatus.OK);
         } else {
             return new ResponseEntity(HttpStatus.UNAUTHORIZED);
