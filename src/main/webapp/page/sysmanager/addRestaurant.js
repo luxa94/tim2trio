36d3a7a7fff113b48fcae196ae3f37fac60b21ae
Nikola Lukić <luknik94@gmail.com>
2016-04-18 21:34:35 +0200

    dodavanje restorana

--- src/main/webapp/page/sysmanager/addRestaurant.js
+++ src/main/webapp/page/sysmanager/addRestaurant.js
@@ -2,12 +2,32 @@ iceipiceApp.controller('sysmanagerAddRestaurantController', function ($scope, $h
     $scope.restaurant = {};
 
     $scope.current.page = 1;
-
+    
+    $scope.typeNames = [];
+    
+    $http.get('/api/restaurantTypes/all').success(function(data) {
+        $scope.typeNames = data;
+        console.log($scope.typeNames);
+        
+    });
+    
     $scope.cancel = function () {
         $scope.restaurant = {};
     };
     
     $scope.submit = function () {
-        
+        $http.post('/api/restaurant/create', $scope.restaurant).success(function (data) {
+            console.log('posted restaurant');
+            alert('Restoran uspesno dodat');
+            $scope.cancel();
+        }).error(function () {
+            console.log('error');
+        });
     };
+
+    // $(function () {
+    //     $('select').multipleSelect();
+    // });
+
+    console.log('done');
 });
