cfb9a70e9cb67550e04aed05782898ab7f49e80e
Nikola Lukić <luknik94@gmail.com>
2016-04-22 13:44:57 +0200

    radi dodavanje restorana

--- src/main/java/rs/isa/mrs/trio/iceipice/Application.java
+++ src/main/java/rs/isa/mrs/trio/iceipice/Application.java
@@ -6,19 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 
-import javax.mail.Authenticator;
-import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.PasswordAuthentication;
-import javax.mail.Session;
-import javax.mail.Transport;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
-
-import java.util.Properties;
-import java.util.Scanner;
-
-
 @SpringBootApplication
 @EnableAutoConfiguration
 public class Application extends SpringBootServletInitializer {
