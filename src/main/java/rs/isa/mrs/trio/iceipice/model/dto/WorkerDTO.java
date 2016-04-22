a543856c0a70c7a65df867d539d758714063d9c5
Aleksandra Milivojevic <aleksandra.milivojevic.94@gmail.com>
2016-04-22 16:31:52 -0700

    asd

---
+++ src/main/java/rs/isa/mrs/trio/iceipice/model/dto/WorkerDTO.java
@@ -0,0 +1,89 @@
+package rs.isa.mrs.trio.iceipice.model.dto;
+
+import java.util.Date;
+
+/**
+ * Created by Sandra on 22.4.2016.
+ */
+public class WorkerDTO {
+
+    private String type;
+
+    private String email;
+
+    private String password;
+
+    private String name;
+
+    private String surname;
+
+    private Date birthDate;
+
+    private String dressSize;
+
+    private String footwearSize;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getDressSize() {
+        return dressSize;
+    }
+
+    public void setDressSize(String dressSize) {
+        this.dressSize = dressSize;
+    }
+
+    public String getFootwearSize() {
+        return footwearSize;
+    }
+
+    public void setFootwearSize(String footwearSize) {
+        this.footwearSize = footwearSize;
+    }
+}
---
+++ src/main/java/rs/isa/mrs/trio/iceipice/services/CookService.java
@@ -0,0 +1,7 @@
+package rs.isa.mrs.trio.iceipice.services;
+
+/**
+ * Created by Sandra on 22.4.2016.
+ */
+public class CookService {
+}
