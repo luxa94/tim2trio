35315bf5f37188b3e1ce551a70bacc7868e41614
Nikola Lukić <luknik94@gmail.com>
2016-04-22 17:15:11 +0200

    prikaz restorana kao tabele

--- src/main/webapp/page/guest/showRestaurants.js
+++ src/main/webapp/page/guest/showRestaurants.js
@@ -2,5 +2,10 @@
  * Created by Nina on 17-Apr-16.
  */
 iceipiceApp.controller('guestShowRestaurantsController', function ($scope, $http, $state, $stateParams, authorizationService) {
+    $scope.restaurants = [];
+    $scope.current.page = 2;
 
+    $http.get('/api/restaurants/all').success(function (data) {
+        $scope.restaurants = data;
+    });
 });
