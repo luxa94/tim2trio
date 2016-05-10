ab1f4c9f92bd9c26bf2ce3d0790caad9f067f1f8
Nina <ninasimicns@gmail.com>
2016-05-10 07:57:48 +0200

    Dizajn dodavanja prijatelja uskladjen tako da bude konzistentan za ostalim dijalozima

--- src/main/java/rs/isa/mrs/trio/iceipice/controllers/TestController.java
+++ src/main/java/rs/isa/mrs/trio/iceipice/controllers/TestController.java
@@ -53,8 +53,11 @@ public class TestController {
             SystemManager sm = new SystemManager("aaa", "aaa", "aaa", "aaa", "asdasd", new Date());
             RestaurantManager rm = new RestaurantManager("rm","rm","rm","rm","123", new Date(),r);
             Bartender br = new Bartender("br","br","Petar","Perić","123", new Date(),"42", "L");
-            Guest g = new Guest("gg", "gg", "gg", "gg", "123", new Date());
-            Guest g1 = new Guest("g1", "g1", "gg", "gg", "123", new Date());
+            Guest g = new Guest("gg", "gg", "Ana", "Anastasijevic", "123", new Date());
+            Guest g1 = new Guest("g1", "g1", "Antonina", "Ninic", "123", new Date());
+            Guest g2 = new Guest("g2", "g2", "Petar", "Peric", "123", new Date());
+            Guest g3 = new Guest("g3", "g3", "Mitar", "Miric", "123", new Date());
+            Guest g4 = new Guest("g4", "g4", "Blaza", "Blazic", "123", new Date());
             Cook ck = new Cook("cook", "cook", "cook", "cook", "123", new Date(), "cook", "M", "41");
             Waiter w = new Waiter("waiter", "waiter", "Marko", "Marković", "123456789", new Date(), "waiter", "XL", "46");
             bartenderRepository.save(br);
@@ -62,6 +65,9 @@ public class TestController {
             systemManagerRepository.save(sm);
             guestRepository.save(g);
             guestRepository.save(g1);
+            guestRepository.save(g2);
+            guestRepository.save(g3);
+            guestRepository.save(g4);
             restaurantManagerRepository.save(rm);
             cookRepository.save(ck);
             waiterRepository.save(w);
