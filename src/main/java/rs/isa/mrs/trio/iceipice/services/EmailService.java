c2bfa65225b4a26069ae834506a8dd95d8594288
Nikola Lukić <luknik94@gmail.com>
2016-06-23 08:27:20 +0200

    he he he

--- src/main/java/rs/isa/mrs/trio/iceipice/services/EmailService.java
+++ src/main/java/rs/isa/mrs/trio/iceipice/services/EmailService.java
@@ -15,39 +15,41 @@ import java.util.Properties;
 public class EmailService {
 
     public void sendMail(BaseUser user) {
-        Properties properties = new Properties();
-        properties.put("mail.smtp.auth", "true");
-        properties.put("mail.smtp.starttls.enable", "true");
-        properties.put("mail.smtp.host", "smtp.gmail.com");
-        properties.put("mail.smtp.port", "587");
+        Thread t = new Thread(() -> {
+            Properties properties = new Properties();
+            properties.put("mail.smtp.auth", "true");
+            properties.put("mail.smtp.starttls.enable", "true");
+            properties.put("mail.smtp.host", "smtp.gmail.com");
+            properties.put("mail.smtp.port", "587");
 
-        final String username = "iceipice.trio@gmail.com";
-        final String password = "fabuloso";
-        final String subject = "Verifikacija lozinke";
+            final String username = "iceipice.trio@gmail.com";
+            final String password = "fabuloso";
+            final String subject = "Verifikacija lozinke";
 
-        final String messageText = String.format("Uspešno ste se ulogovali na http://localhost:8080/#/authenticate/%d. \nMolimo Vas da klikom na link iznad potvrdite svoju lozinku.", user.getId());
+            final String messageText = String.format("Uspešno ste se ulogovali na http://localhost:8080/#/authenticate/%d. \nMolimo Vas da klikom na link iznad potvrdite svoju lozinku.", user.getId());
 
-        Session session = Session.getDefaultInstance(properties, new Authenticator() {
-            @Override
-            protected PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication(username, password);
+            Session session = Session.getDefaultInstance(properties, new Authenticator() {
+                @Override
+                protected PasswordAuthentication getPasswordAuthentication() {
+                    return new PasswordAuthentication(username, password);
+                }
+            });
+
+            try {
+                Message message = new MimeMessage(session);
+                message.setFrom(new InternetAddress(username));
+                message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(user.getEmail()));
+                message.setSubject(subject);
+                message.setText(messageText);
+                Transport.send(message);
+
+                System.out.println("Message sent!");
+            } catch (MessagingException e) {
+                e.printStackTrace();
             }
         });
-
-        try {
-            Message message = new MimeMessage(session);
-            message.setFrom(new InternetAddress(username));
-            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(user.getEmail()));
-            message.setSubject(subject);
-            message.setText(messageText);
-            Transport.send(message);
-
-            System.out.println("Message sent!");
-        } catch (MessagingException e) {
-            e.printStackTrace();
-        }
+        t.start();
     }
-
 
 
 }
